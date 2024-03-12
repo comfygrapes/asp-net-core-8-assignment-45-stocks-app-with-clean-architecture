d4b7a9d301006b014e2eb0695583edd8639ec22b
comfygrapes <76193542+comfygrapes@users.noreply.github.com>
2024-03-12 16:32:28 +0800

    Add project files.

---
+++ StocksApp.UI/Components/SelectedStockViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceContracts;
+
+namespace StocksApp.Components
+{
+    public class SelectedStockViewComponent : ViewComponent
+    {
+        private readonly IFinnhubCompanyProfileService _finnhubCompanyProfileService;
+        private readonly IFinnhubStockPriceQuoteService _finnhubStockPriceQuoteService;
+
+        public SelectedStockViewComponent(IFinnhubCompanyProfileService finnhubCompanyProfileService, IFinnhubStockPriceQuoteService finnhubStockPriceQuoteService)
+        {
+            _finnhubCompanyProfileService = finnhubCompanyProfileService;
+            _finnhubStockPriceQuoteService = finnhubStockPriceQuoteService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string stockSymbol)
+        {
+            var companyProfile = await _finnhubCompanyProfileService.GetCompanyProfile(stockSymbol) ?? new();
+            var stock = await _finnhubStockPriceQuoteService.GetStockPriceQuote(stockSymbol) ?? new();
+
+            
+            if (stock.TryGetValue("c", out var currentPrice))
+            {
+                companyProfile.Add("price", currentPrice);
+            }
+
+            return View(companyProfile);
+        }
+    }
+}
---
+++ StocksApp.UI/Program.cs
@@ -0,0 +1,48 @@
+using Entities;
+using RepositoryContracts;
+using ServiceContracts;
+using Services;
+using StocksApp.Options;
+using Serilog;
+using StocksApp.Middleware;
+using StocksApp.StartupExtensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog();
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Configuration.AddUserSecrets<Repositories.FinnhubRepository>();
+}
+
+builder.Services.ConfigureServices(builder.Configuration);
+
+var app = builder.Build();
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .ReadFrom.Services(app.Services)
+    .WriteTo.Console()
+    .CreateLogger();
+
+if (builder.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandlingMiddleware();
+}
+
+if (!builder.Environment.IsEnvironment("Test"))
+    Rotativa.AspNetCore.RotativaConfiguration.Setup("wwwroot", wkhtmltopdfRelativePath: "rotativa");
+
+app.UseStaticFiles();
+app.UseRouting();
+app.MapControllers();
+
+app.Run();
+
+public partial class Program { }
